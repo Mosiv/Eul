47e64f3d0d849eff4ae567ac33988e19336b3c5a
Mosiv <aqlpp1@yandex.ru>
2020-05-13 23:25:28 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,7 +3,8 @@ a = []
 for i in range(100,1000):
     for n in range(100,1000):
         pal = str(i*n)
-        if (pal == pal[::-1]) and (int(pal) > 900000):
-            a.append(pal)
-            print(a)
-print(pal)
+        if (pal == pal[::-1]):
+            a.append(int(pal))
+            break
+print(a)
+print(max(a))
